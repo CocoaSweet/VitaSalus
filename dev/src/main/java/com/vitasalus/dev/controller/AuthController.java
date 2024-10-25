a1b6d33e1a1841e6eebf60d6e80d7d29be91cd46
CocoaSweet <li.piva@hotmail.com>
2024-10-24 22:02:14 -0300

    cors configure

--- dev/src/main/java/com/vitasalus/dev/controller/AuthController.java
+++ dev/src/main/java/com/vitasalus/dev/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.vitasalus.dev.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.vitasalus.dev.service.impl.MedicoServiceImpl;
 import com.vitasalus.dev.service.impl.PacienteServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/auth")
 public class AuthController {
 	
